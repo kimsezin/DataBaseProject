aaccdb8899bbd4b61346a495ffafc71ef41a3e67
kimsezin <kimsejin15@naver.com>
2020-12-14 22:05:25 +0900

    Project

--- Project/Project.py
+++ Project/Project.py
@@ -78,7 +78,8 @@ def Pet():
         db.execute(
             'delete from Pet where Pet_Number = ?;',
             (request.form['Pet Number'])
-        )
+        ).fetchall()
+
         db.commit()
         return render_template('PetOK.html')
     else:
@@ -90,7 +91,32 @@ def Pet():
         db.close()
         return render_template('Pet.html', Pet_List=Pet_List)
 
-    
+@app.route("/Pet_Register", methods=['POST', 'GET'])
+def Pet_Register():
+    db = sqlite3.connect('/home/kimsezin/DatabaseProject/Project/PetShop.db')
+    db.row_factory = sqlite3.Row
+    if request.method == 'POST' :
+        db.execute(
+            'Insert into Pet values(?,?,?,?,?);',
+            (request.form['Pet Number'],request.form['Species'],
+            request.form['Age'], request.form['Sex'],
+            request.form['Center Number'])
+        )
+        db.commit()     
+
+        db.close() 
+        return render_template('OK_Pet.html') 
+
+    else :    
+        LastNumber_item = db.execute(
+            'select Pet_Number '
+            ' from (select Pet_Number from Pet order by Pet_Number desc limit 1)'
+            ' order by Pet_Number asc'
+        ).fetchone()
+
+        LastNumber = ord(LastNumber_item['Pet_Number'])-47 
+
+        return render_template('Pet_Register.html', LastNumber=LastNumber)    
     
 if __name__ == "__main__":
     app.debug=True
